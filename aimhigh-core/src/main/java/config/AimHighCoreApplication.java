4bfc2bd39a05cde5b1b9b8097c7ddeddcdf5fa8e
lego <mincheol6073@gmail.com>
2024-04-04 15:02:08 +0900

    [프로젝트 디자인 및 개발 환경 구성] 개발 디자인 수정, ERD 설계, 소프트웨어 아키텍처 설계

---
+++ aimhigh-core/src/main/java/config/AimHighCoreApplication.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimHighCoreApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AimHighCoreApplication.class, args);
+  }
+}
